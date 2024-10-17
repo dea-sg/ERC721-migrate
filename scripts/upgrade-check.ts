bb1ed361f00510ea195548aba72476689a34723f
“Akira-Taniguchi” <dededededaiou2003@yahoo.co.jp>
2024-10-17 16:41:46 +0900

    add

--- scripts/upgrade-check.ts
+++ scripts/upgrade-check.ts
@@ -4,13 +4,15 @@ import { ethers } from 'hardhat'
 
 async function main() {
 	// アドレス確認！！！！
-	const proxyAddress = '0xb0071322db5fcda6dda03ae456524c3e5e5d07a3'
+	// const proxyAddress = '0xb0071322db5fcda6dda03ae456524c3e5e5d07a3'
+	const proxyAddress = '0x1f40cc97b4d5163eef61466859ce531c609cc492'
 	const tokenFactory = await ethers.getContractFactory(
 		'ERC721TokenUpgradableV2'
 	)
 	const token = tokenFactory.attach(proxyAddress)
 	const filter = token.filters.Upgraded()
 	const events = await token.queryFilter(filter)
+	console.log(events.length)
 	console.log('implementation to:', events[0].args!.implementation)
 	console.log('implementation to:', events[1].args!.implementation)
 	console.log('implementation to:', events[2].args!.implementation)
--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -2,7 +2,8 @@ import { ethers, upgrades } from 'hardhat'
 
 async function main() {
 	// アドレス確認！！！！
-	const proxyAddress = '0xb0071322db5fcda6dda03ae456524c3e5e5d07a3'
+	// const proxyAddress = '0xb0071322db5fcda6dda03ae456524c3e5e5d07a3'
+	const proxyAddress = '0x1f40cc97b4d5163eef61466859ce531c609cc492'
 	const tokenFactoryOld = await ethers.getContractFactory(
 		'ERC721TokenUpgradable'
 	)
